97f9838e87300ff8076234ba1eff2eeb5b8b0357
Wesley Reisz <wes@wesleyreisz.com>
2013-10-27 12:11:13 -0400

    Wired the activity up to display the fragmentlist.

--- files/src/com/example/classlab7c/MainActivity.java
+++ files/src/com/example/classlab7c/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.Menu;
 
 import com.example.classlab7c.fragments.Layout1;
+import com.example.classlab7c.fragments.Layout2;
+import com.example.classlab7c.fragments.Layout3;
+import com.example.classlab7c.fragments.Layout4;
 import com.example.classlab7c.listeners.OnItemSelectedListener;
 
 public class MainActivity extends Activity implements OnItemSelectedListener{
@@ -32,8 +35,18 @@ public class MainActivity extends Activity implements OnItemSelectedListener{
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         
         Layout1 layout1;
-        
+        Layout2 layout2;
+        Layout3 layout3;
+        Layout4 layout4;
 		
+        //I do this because a string literal can never be null, the string
+        //passed in can be. It's a very simple way of protecting your code
+        //from npe -wtr
+        if("layout1".equalsIgnoreCase(link)){
+        	layout1 = new Layout1();
+        	fragmentTransaction.replace(R.id.detailFragment, layout1);  
+            fragmentTransaction.commit(); 
+        }
 	}
     
 }
