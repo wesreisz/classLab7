e973cd3aa00237c67292400552c62828524d94b5
Wesley Reisz <wes@wesleyreisz.com>
2013-10-27 17:11:27 -0400

    initial adapter and list bind

--- files/src/com/example/classlab7c/fragments/ListFragment.java
+++ files/src/com/example/classlab7c/fragments/ListFragment.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.classlab7c.R;
 import com.example.classlab7c.listeners.OnItemSelectedListener;
@@ -19,6 +21,14 @@ public class ListFragment extends Fragment {
 	private OnItemSelectedListener listener;
 	private List<MenuItem>menuItems;
 	
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -35,6 +45,12 @@ public class ListFragment extends Fragment {
 				updateDetail("layout1");  
 			}
 		});
+		
+		ArrayAdapter<MenuItem> adapter =
+				new ArrayAdapter<MenuItem>(getActivity(), android.R.layout.simple_list_item_1, menuItems);
+		ListView listViewMusic = (ListView) view.findViewById(R.id.listViewMusic);
+		listViewMusic.setAdapter(adapter);
+		
 		return view;
 	}
 	
