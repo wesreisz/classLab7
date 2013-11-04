21028a8e26d1a2ce4c47d73f165f034a2c17c533
Wesley Reisz <wes@wesleyreisz.com>
2013-11-03 22:26:26 -0500

    change some color and passed layout4 as initial startup screen

--- files/src/com/example/classlab7c/MainActivity.java
+++ files/src/com/example/classlab7c/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends Activity implements OnItemSelectedListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onRssItemSelected("layout4");
     }
 
 
